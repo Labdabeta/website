37da0272ccc7ba04bd86019e6f36559dcaa12c49
Louis Burke <lburke@bell.net>
2016-04-28 12:53:47 -0400

    Fixed copyright positioning

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,6 @@
 // Adds the copyright notice to the page
-$(document.body).append("<div class=\"copyright\">&copy; 2016 Louis Burke </div>");
+$(document.body).append(
+        "<div class=\"footer\">" +
+            "<a class=\"contact\" href=\"mailto:lburke@labprogramming.net?Subject=Website\">Contact me!</a>"+
+            "<div class=\"copyright\">&copy; 2016 Louis Burke </div>" + 
+        "</div>");
